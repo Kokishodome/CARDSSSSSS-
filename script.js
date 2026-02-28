505188b07bd0ea71ecd4417f7fddf4ff903f2b16
slay <159949398+Kokishodome@users.noreply.github.com>
2026-02-28 12:58:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let cards = document.getElementById('cards')
+
+for (let i = 0; i < 24; i++) {
+    let img = document.createElement('img')
+    img.src = 'download (1).jfif'
+    cards.appendChild(img)
+}
