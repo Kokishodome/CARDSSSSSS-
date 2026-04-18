bf8a8b62e58182e8fa497aea91f0fbf38204f8e6
slay <159949398+Kokishodome@users.noreply.github.com>
2026-04-18 10:38:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let cards = document.getElementById('cards')
 let pair = []
+let attempts = document.getElementById('attempts')
+let attemptsCounter = 0
+let goodLuck = document.getElementById('goodLuck')
 let images = ['stop.jfif',
     'sixseven.jfif',
     'cat.jfif',
@@ -26,22 +29,50 @@ let images = ['stop.jfif',
     'twtwocute.jfif'
 ]
 
+images.sort(function () {
+    return Math.random() - 0.5
+})
+
+
 for (let i = 0; i < 24; i++) {
     let img = document.createElement('img')
     img.src = 'download (1).jfif'
     cards.appendChild(img)
     img.onclick = function () {
-        console.log(i);
-        img.src = images[i]
-        pair.push(img)
+        img.style.transform = 'scaleX(0)'
+        setTimeout(() => {
+            img.style.transform = 'scaleX(1)'
+            img.src = images[i]
+        }, 300);
+        console.log(pair[0], img, pair[0] === img);
+        if (pair[0] !== img) {
+            pair.push(img)
+        }
         console.log(pair);
         if (pair.length == 2) {
-            setTimeout(() => {
-                pair[0].src = 'download (1).jfif'
-                pair[1].src = 'download (1).jfif'
+            attemptsCounter++
+            attempts.innerHTML = 'attempts: ' + attemptsCounter 
+            if (pair[0].src == pair[1].src) {
+                pair = []
+            }
+            else {
+                cards.style.pointerEvents = 'none'
+                setTimeout(() => {
+                    pair[0].style.transform = 'scaleX(0)'
+                    pair[1].style.transform = 'scaleX(0)'
+                    setTimeout(() => {
+                        pair[0].style.transform = 'scaleX(1)'
+                        pair[0].src = 'download (1).jfif'
+                        pair[1].style.transform = 'scaleX(1)'
+                        pair[1].src = 'download (1).jfif'
+                        cards.style.pointerEvents = 'auto'
+                        pair = []
+                    }, 300);
 
-            }, 1000);
+                }, 1000);
+            }
         }
     }
 }
 
+// make sure that once the first atempt is made, put on the site to make it visible :)
